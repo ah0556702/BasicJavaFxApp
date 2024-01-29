c40fc17a785d06ed61389c48e2306ba5bf53bb7f
ah0556702 <ah0556702@otc.edu>
2024-01-29 14:22:43 -0600

    Updates made to FXML and main method

--- src/main/java/org/example/basicjavafxapp/HelloApplication.java
+++ src/main/java/org/example/basicjavafxapp/HelloApplication.java
@@ -5,19 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.paint.Color.color;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 426, 608);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 608);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        scene.setFill(Color.web("#81c483"));
     }
 
 
